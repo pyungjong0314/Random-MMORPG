30ccd83eae2fcad4325d2c96b5cc640649fbe8da
pyungjong <sonopj@gmail.com>
2025-05-20 16:37:34 +0900

    feat: character Build + Factory Method

---
+++ WindowsFormsApp1/GAME/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Game.Characters;
+
+namespace WindowsFormsApp1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            /*Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());*/
+
+            Character c1 = CharaceterFactory.CharacterCreate("짱구");
+            Character c2 = CharaceterFactory.CharacterCreate("철수");
+            Character c3 = CharaceterFactory.CharacterCreate("훈이");
+
+            
+            Console.WriteLine(c1.ToString());
+            Console.WriteLine(c2.ToString());
+            Console.WriteLine(c3.ToString());
+            Console.ReadLine();
+        }
+    }
+}
